ce5d3c75f64c44d59f65e66655ce08e5d36c4891
Ryan Sprott <rds2184@tc.columbia.edu>
2018-07-10 15:03:43 -0400

    Get data from API.

---
+++ app/javascript/appService.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+//import { resolve } from 'path';
+
+axios.defaults.baseURL = "https://api.fullstackweekly.com"
+
+const appService = {
+  getPosts (categoryId) {
+    return new Promise((resolve) => {
+      axios.get(`/wp-json/wp/v2/posts?categories=${categoryId}&per_page=6`)
+      .then(response => {
+        resolve(response.data)
+      })  
+    })
+  }
+}
+
+export default appService
