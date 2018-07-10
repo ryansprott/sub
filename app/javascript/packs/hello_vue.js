af830797b9d7ce5eb61db8e9a1491c554db7bf64
Ryan Sprott <rds2184@tc.columbia.edu>
2018-07-10 11:15:14 -0400

    Cleanup.

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -9,15 +9,12 @@ import Vue from 'vue'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'))
+  const el = document.querySelector("#vue-app")
   const app = new Vue({
     el,
     render: h => h(App)
   })
-
-  console.log(app)
 })
-
 
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
