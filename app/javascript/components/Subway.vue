1a2d44ed7d96e19908f5d467d91a07ec23da5046
Ryan Sprott <rds2184@tc.columbia.edu>
2018-07-12 13:10:51 -0400

    Layout.

--- app/javascript/components/Subway.vue
+++ app/javascript/components/Subway.vue
@@ -2,17 +2,21 @@
   <div>    
     <div class="select is-rounded is-large is-fullwidth">
       <select :value="getCurrentStop" @change="fetchArrivalsFromApi($event.target.value)">
-        <option v-for="stop in getAllStops" :value="stop[1]" v-bind:key="stop[0]">
+        <option v-for="(stop, index) in getAllStops" :value="stop[1]" v-bind:key="index">
           {{stop[2] + ' - ' + stop[3]}}
         </option>
       </select>
     </div>
-    <h1>Northbound</h1>
-    <div v-for="stop in getNorthboundArrivals" v-bind:key="stop[3]">
+    <hr />
+    <h1 class="title is-3">Northbound</h1>
+    <div v-for="(stop, index) in getNorthboundArrivals" v-bind:key="index">
+      <h2 class="title is-5">{{index}}</h2>
       {{stop}}
     </div>
-    <h1>Southbound</h1>
-    <div v-for="stop in getSouthboundArrivals" v-bind:key="stop[3]">
+    <hr />
+    <h1 class="title is-3">Southbound</h1>
+    <div v-for="(stop, index) in getSouthboundArrivals" v-bind:key="index">
+      <h2 class="title is-5">{{index}}</h2>
       {{stop}}
     </div>
   </div>
