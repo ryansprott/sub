b6c01f46a2596d90d473e5840565c0071e854a18
Ryan Sprott <rds2184@columbia.edu>
2019-05-31 10:28:12 -0400

    Bumped versions.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180713160333) do
+ActiveRecord::Schema.define(version: 2018_07_13_160333) do
 
   create_table "bus_stops", force: :cascade do |t|
     t.string "code"
