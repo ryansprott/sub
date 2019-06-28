268aeed29c2c89416552e0d1c8e5faad8ec146e0
Ryan Sprott <rds2184@columbia.edu>
2019-06-28 14:28:39 -0400

    Stuff

--- app/javascript/components/equipment/Equipment.vue
+++ app/javascript/components/equipment/Equipment.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-for="(item, ndx) in getEquipmentStatus" :key="ndx" class="content">
-      <h1>{{ndx}}</h1>
+      <h1>{{boroughHelper(ndx)}}</h1>
       <div v-for="(status, i) in item" :key="i">
         <div class="card">
           <header class="card-header">
@@ -14,7 +14,11 @@
             </div>
           </div>
           <footer class="card-footer">
-            <div class="card-footer-item">{{status.trains}}</div>
+            <div class="card-footer-item">
+              <span v-for="(train, n) in status.trains" :key="n" :class="'bullet train-' + train">
+                {{train}}
+              </span>
+            </div>
             <div class="card-footer-item">{{status.reason}}</div>
           </footer>
         </div>
@@ -37,6 +41,19 @@ export default {
   methods: {
     refreshEquipmentStatus() {
       this.$store.dispatch('fetchEquipmentStatusFromApi')
+    },
+    boroughHelper(abbrev) {
+      if (abbrev === "MN") {
+        return "Manhattan"
+      } else if (abbrev === "BX") {
+        return "The Bronx"
+      } else if (abbrev === "QNS") {
+        return "Queens"
+      } else if (abbrev === "BKN") {
+        return "Brooklyn"
+      } else {
+        return "Dunno"
+      }
     }
   },
   mounted() {
@@ -48,3 +65,47 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .bullet {
+    color: white;
+    border-radius: 6em;
+    min-width: 1.5em;
+    text-align: center;
+    font-weight: bolder;
+    margin-right: 0.15em;
+  }
+
+  .train-1, .train-2, .train-3 {
+    background: red;
+  }
+
+  .train-4, .train-5, .train-6 {
+    background: green;
+  }
+
+  .train-7 {
+    background: purple;
+  }
+
+  .train-A, .train-C, .train-E {
+    background: blue;
+  }
+
+  .train-B, .train-D, .train-F, .train-M {
+    background: orange;
+  }
+
+  .train-N, .train-Q, .train-R, .train-W {
+    background: yellow;
+  }
+
+  .train-G {
+    background: lightgreen;
+  }
+
+  .train-J, .train-Z {
+    background: brown;
+  }
+
+</style>
